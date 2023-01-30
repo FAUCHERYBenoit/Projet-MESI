5d9b437f7d4d0f8a026d14ca8ddc9116b1a71689
Vladimir Chantitch <chantitch.vladimir@gmail.com>
2023-01-30 10:20:27 +0100

    WIP

--- Assets/Scripts/Character/AnimatorManager.cs
+++ Assets/Scripts/Character/AnimatorManager.cs
@@ -4,5 +4,27 @@ using UnityEngine;
 
 public class AnimatorManager : MonoBehaviour
 {
-    
+    [SerializeField] Animator animator;
+
+    public void StartRunning()
+    {
+        animator.SetFloat("Speed", 1);
+    }
+
+    public void StopRunnning()
+    {
+        animator.SetFloat("Speed", 0);
+    }
+
+    public void SpecialMovement()
+    {
+        animator.CrossFade("SpecialMovement", 0.15f);
+        animator.SetBool("IsSpecialMotion", true);
+        animator.Play("SpecialMovement");
+    }
+
+    public void StopSpecialMotion()
+    {
+        animator.SetBool("IsSpecialMotion", false);
+    }
 }
